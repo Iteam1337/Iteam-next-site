02cef0e39e94f5346800bfc0dcf95eb977ee19ca
Christian Landgren <christian@landgren.nu>
2021-06-01 12:43:53 +0200

    feat: added logos

--- src/sections/startpage/Clients.js
+++ src/sections/startpage/Clients.js
@@ -8,7 +8,12 @@ import imgL1Brand1 from "../../assets/image/svg/vimla.svg";
 import imgL1Brand2 from "../../assets/image/svg/seb.svg";
 import imgL1Brand3 from "../../assets/image/svg/taxi-stockholm.svg";
 import imgL1Brand4 from "../../assets/image/png/Skatteverket.png";
-import imgL1Brand5 from "../../assets/image/png/weopendata.png";
+import imgL1Brand5 from "../../assets/image/svg/arbetsformedlingen.svg";
+import imgL1Brand6 from "../../assets/image/png/weopendata.png";
+import imgL1Brand7 from "../../assets/image/svg/gavagai.svg";
+import imgL1Brand8 from "../../assets/image/png/tjejjouren.png";
+import imgL1Brand9 from "../../assets/image/png/swedbank.png";
+import imgL1Brand10 from "../../assets/image/png/cdt.png";
 
 const Brand = styled(Box)`
   opacity: 0.7;
@@ -44,6 +49,22 @@ const Clients = () => (
           </Brand>
           <Brand className="" py={3} mx={4}>
             <img src={imgL1Brand5} alt="" className="img-fluid" />
+          </Brand>
+
+          <Brand className="" py={3} mx={4}>
+            <img src={imgL1Brand6} alt="" className="img-fluid" />
+          </Brand>
+          <Brand className="" py={3} mx={4}>
+            <img src={imgL1Brand7} alt="" className="img-fluid" />
+          </Brand>
+          <Brand className="" py={3} mx={4}>
+            <img src={imgL1Brand8} alt="" className="img-fluid" />
+          </Brand>
+          <Brand className="" py={3} mx={4}>
+            <img src={imgL1Brand9} alt="" className="img-fluid" />
+          </Brand>
+          <Brand className="" py={3} mx={4}>
+            <img src={imgL1Brand10} alt="" className="img-fluid" />
           </Brand>
         </Box>
       </Container>
