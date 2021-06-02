405ee04710db068f7d41872716484cfb07d14f86
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-06-02 12:16:34 +0200

    add case items to json

---
+++ src/data/caseItems.js
@@ -0,0 +1,34 @@
+export const caseItems = [
+  {
+    title: "Tjejjouren Väst",
+    subTitle: "Appen Stella ger tjejer stöd dygnet runt",
+    link: "/case/tjejjouren-vast",
+    img: require("../assets/image/jpeg/case-card-img-2.jpg"),
+  },
+  {
+    title: "TRR",
+    subTitle: " Vi rustar Sveriges bästa omställningsaktör för framtiden",
+    link: "/case/trr",
+    img: require("../assets/image/case/trr-case-phone.png"),
+  },
+  {
+    title: "Matcha kontraktet",
+    subTitle:
+      "Tryggare boendemiljö med automatisk spårning av olaglig andrahandsuthyrning",
+    link: "/case/matcha-kontraktet",
+    img: require("../assets/image/case/matcha-kontraktet1.jpg"),
+  },
+  {
+    title: "Skellefteå ansiktsigenkänning",
+    subTitle:
+      " Kan man använda ansiktsigenkänning för att registrera närvaro i en skola? Vi hjälpte Skellefteå att försöka hitta en teknisk lösning som skyddade integriteten.",
+    link: "/case/skelleftea",
+    img: require("../assets/image/jpeg/case-card-img-3.jpg"),
+  },
+  {
+    title: "Skatteverket & Karios Future",
+    subTitle: "Säkra kvitton genom kryptoteknik",
+    link: "/case/skatteverket-kairos-future",
+    img: require("../assets/image/case/Digitala_kvitton.png"),
+  },
+];
--- src/sections/case/CaseList1.js
+++ src/sections/case/CaseList1.js
@@ -1,73 +1,24 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
+import { caseItems } from "../../data/caseItems";
 import { Section } from "../../components/Core";
 import PostCard from "../../components/PostCard";
 
-// TODO: add json with case
-import trrThumbnail from "../../assets/image/case/trr-case-phone.png";
-import imgCase1 from "../../assets/image/case/matcha-kontraktet1.jpg";
-import imgCase2 from "../../assets/image/jpeg/case-card-img-2.jpg";
-import imgCase3 from "../../assets/image/jpeg/case-card-img-3.jpg";
-import imgCase4 from "../../assets/image/case/Digitala_kvitton.png";
-import imgBrand1 from "../../assets/image/jpeg/case-card-brand-img.jpg";
-import imgBrand2 from "../../assets/image/jpeg/case-card-brand-img-2.jpg";
-import imgBrand3 from "../../assets/image/jpeg/case-card-brand-img-3.jpg";
-
 const Column = (props) => <Col sm={6} lg={4} className="mb-4" {...props} />;
-
 const CaseList = () => (
   <>
     {/* <!-- Feature section --> */}
     <Section className="position-relative">
       <Container>
         <Row>
-          <Column>
-            <PostCard
-              img={imgCase2}
-              title="Tjejjouren Väst"
-              link="/case/tjejjouren-vast"
-            >
-              Appen Stella ger tjejer stöd dygnet runt
-            </PostCard>
-          </Column>
-          <Column>
-            <PostCard img={trrThumbnail} title="TRR" link="/case/trr">
-              Vi rustar Sveriges bästa omställningsaktör för framtiden
-            </PostCard>
-          </Column>
-          <Column>
-            <PostCard
-              img={imgCase1}
-              imgBrand={imgBrand1}
-              title="Matcha kontraktet"
-              link="/case/matcha-kontraktet"
-            >
-              Tryggare boendemiljö med automatisk spårning av olaglig
-              andrahandsuthyrning
-            </PostCard>
-          </Column>
-          <Column>
-            <PostCard
-              img={imgCase3}
-              imgBrand={imgBrand3}
-              title="Skellefteå ansiktsigenkänning"
-              link="/case/skelleftea"
-            >
-              Kan man använda ansiktsigenkänning för att registrera närvaro i en
-              skola? Vi hjälpte Skellefteå att försöka hitta en teknisk lösning
-              som skyddade integriteten.
-            </PostCard>
-          </Column>
-          <Column>
-            <PostCard
-              img={imgCase4}
-              title="Skatteverket & Karios Future"
-              link="/case/skatteverket-kairos-future"
-            >
-              Säkra kvitton genom kryptoteknik
-            </PostCard>
-          </Column>
+          {caseItems.map((item, i) => (
+            <Column key={i}>
+              <PostCard img={item.img} title={item.title} link={item.link}>
+                {item.subTitle}
+              </PostCard>
+            </Column>
+          ))}
         </Row>
       </Container>
     </Section>
