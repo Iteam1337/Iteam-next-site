c363fbf5fd2ad41f566f5dbe1ca1f101ce2dc85a
izabellalarsson <izzwii.izabella@gmail.com>
2021-06-02 11:44:08 +0200

    chore: add form to iteamX page

---
+++ src/components/HubspotForm/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+
+import { Title, Box } from "../Core";
+
+import { device } from "../../utils";
+
+const FormStyled = styled.form`
+  padding: 40px 30px;
+  @media ${device.sm} {
+    padding: 53px 58px 50px;
+  }
+  box-shadow: ${({ theme }) => `0 20px 61px ${theme.colors.shadow}`};
+  border-radius: 10px;
+  background-color: #f7f7fb;
+  width: 100%;
+`;
+
+const HubspotForm = ({
+  value,
+  title = "Boka intromöte direkt i kalendern",
+}) => {
+  React.useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <FormStyled
+      name={value}
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <Box>
+        <Title>{title}</Title>
+      </Box>
+      <input type="hidden" name="form-name" value={value} />
+      <div
+        class="meetings-iframe-container"
+        data-src="https://meetings.hubspot.com/jonna-hjern/intromote-iteam?embed=true"
+      />
+    </FormStyled>
+  );
+};
+
+export default HubspotForm;
