acebd40764c37659098799e73f68351500fedce2
Izabella Larsson <izzwii.izabella@gmail.com>
2021-06-02 13:10:21 +0200

    Feat: hubspot form (#15)
    * chore: add form to iteamX page
    
    * chore: add form to Iteam Mvp page

--- src/sections/iteamX/Contact.js
+++ src/sections/iteamX/Contact.js
@@ -1,20 +1,13 @@
 import React from "react";
-import Link from "next/link";
 import styled from "styled-components";
 import { Container, Row, Col } from "react-bootstrap";
 
-import {
-  Title,
-  Button,
-  Box,
-  Text,
-  Input,
-  Checkbox,
-} from "../../components/Core";
+import { Box } from "../../components/Core";
 
 import { device } from "../../utils";
 
 import imgShape from "../../assets/image/svg/contact-shape.svg";
+import HubspotForm from "../../components/HubspotForm";
 
 const BoxStyled = styled(Box)`
   &::after {
@@ -40,22 +33,6 @@ const Shape = styled(Box)`
   }
 `;
 
-const FormStyled = styled.form`
-  padding: 40px 30px;
-  @media ${device.sm} {
-    padding: 53px 58px 50px;
-  }
-  box-shadow: ${({ theme }) => `0 20px 61px ${theme.colors.shadow}`};
-  border-radius: 10px;
-  background-color: #f7f7fb;
-  width: 100%;
-`;
-
-const AForgot = styled.a`
-  color: ${({ theme }) => theme.colors.secondary} !important;
-  text-decoration: none !important;
-`;
-
 const Contact = () => (
   <>
     <BoxStyled
@@ -75,41 +52,13 @@ const Contact = () => (
       >
         <Row className="justify-content-center">
           <Col
-            lg="9"
-            xl="8"
+            lg="10"
             data-aos="fade-up"
             data-aos-duration="750"
             data-aos-once="true"
             data-aos-delay="500"
           >
-            <FormStyled>
-              <div className="mb-5 text-center">
-                <Title className="mb-2">Let’s talk</Title>
-                <Text>Drop us a line & we will get back to you soon!</Text>
-              </div>
-
-              <Box mb={3}>
-                <Input type="text" placeholder="Your name" />
-              </Box>
-              <Box mb={3}>
-                <Input type="email" placeholder="Email address" />
-              </Box>
-              <Box mb={4} className="position-relative">
-                <Input as="textarea" placeholder="Tell us about your project" />
-              </Box>
-              <Box mb={3} className="text-left">
-                <Checkbox>
-                  I agree to the{" "}
-                  <Link href="/">
-                    <AForgot>Terms & Condition</AForgot>
-                  </Link>
-                </Checkbox>
-              </Box>
-
-              <Button width="100%" type="submit" borderRadius={10}>
-                Send
-              </Button>
-            </FormStyled>
+            <HubspotForm value="iteamX" title="Boka ett intromöte" />
           </Col>
         </Row>
       </Container>
