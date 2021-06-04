4da7b897f3937aa080506602146ae56f56ff5fe3
Johanna Grahn <johanna.grahn@iteam.se>
2021-06-04 11:34:15 +0200

    fix i head of och lagt till backend dev

--- src/pages/career/head-of-development.js
+++ src/pages/career/head-of-development.js
@@ -35,27 +35,23 @@ const OpeningDetails = () => {
                     Iteam söker Head of Development!
                   </Title>
                   <Text variant="small">
-                    Iteam kommer ur pandemin med nya krafter och hopp om
-                    framtiden! Vi vet att världen är i ett viktigt skifte och
-                    att vi kan bidra genom vår metod för att lyckas med komplexa
+                    Vi vet att världen är i ett viktigt skifte och att vi kan 
+                    bidra. Det gör vi genom vår metod för att lyckas med komplexa
                     projekt, genom att ta tillvara på egen innovation och
                     lärande samt genom att hjälpa våra kunder samverka och
                     engagera med open source-utveckling.
                     <br />
                     <br />
-                    Under våren har vi rekryterat två nya utvecklare och två
-                    UX:are och förutom den här tjänsten söker vi också efter en
-                    senior backend-utvecklare. Vi har också startat två riktigt
-                    spännande samarbeten, med en streaming-tjänst i Stockholm
-                    och digital transporttjänst i Göteborg, och håller dessutom
-                    på att köra ett par sista piloter med Predictive Movement
-                    som är ett Vinnova-finansierat projekt där vi på Iteam
-                    bidrar med en AI-baserad tjänst för att samordna transporter
-                    i glesbygd.
+                    Under våren har vi rekryterat fyra nya utvecklare och UX:are.
+                    Vi har också startat två riktigt 
+                    spännande nya samarbeten, med en streaming-tjänst i Stockholm 
+                    och digital transporttjänst i Göteborg.  I Vinnova-projektet 
+                    Predictive Movement arbetar vi med en AI-baserad tjänst för 
+                    att samordna transporter i glesbygd. 
                     <br />
                     <br />
-                    Nu söker vi dig som kommer vara en nyckelperson för att
-                    fortsätta vår tradition att vara en partner i teknisk och
+                    Nu söker vi dig som kommer vara en nyckelperson för att Iteam 
+                    ska fortsätta vara en  partner i teknisk och
                     strategisk framkant, ett måste för hållbara digitala
                     lösningar i ett samhälle av idag.
                     <br />
@@ -64,8 +60,9 @@ const OpeningDetails = () => {
                     företag med högt i tak, familjär stämning och ett brinnande
                     intresse för teknik & människa, framtiden och ett hållbart
                     samhälle. Idag är vi 24 personer utspridda i Sverige och
-                    Paris. Distribuerade team har vi haft sedan 2016 men du
-                    kommer att vara situerad i Stockholm eller Göteborg.
+                    Paris. Distribuerade team har vi haft sedan 2016 och du kan 
+                    välja att tillhöra något av våra kontor i Stockholm eller 
+                    Göteborg, eller också arbeta från en annan ort i Sverige.
                   </Text>
                 </Box>
                 <ImgContainer
@@ -102,86 +99,95 @@ const OpeningDetails = () => {
                     applikationer med hjälp av den senaste tekniken. Du
                     utforskar och utvärderar nya tekniker och metoder, sprider
                     din kunskap och omsätter den i praktiken - i ditt dagliga
-                    arbete, på kodluncher och genom mentorskap. Vår tech-stack
-                    är primärt Open Source (ex Node.js, React, Docker, Elixir,
-                    Python och Go). Vana vid automatisering och automatiserad
-                    testning är ett måste i våra uppdrag!
+                    arbete och genom mentorskap. Vår tech-stack är i sin helhet 
+                    Node.js, React, ReactNative, Typescript, Docker, Kubernetes, 
+                    ElasticSearch, GraphQL, PostgreSQL, Elixir, RabbitMQ, men 
+                    ser olika ut i våra olika team. Vana vid automatisering och 
+                    automatiserad testning är ett måste i alla våra uppdrag!
                   </Text>
                 </Box>
                 <Box pb={["40px", null, "65px"]}>
                   <Title variant="card" mb="18px">
-                    Krav
+                    Du behöver
                   </Title>
-                  <Text variant="small">
-                    <ul style={{ listStyle: "disc", marginLeft: "3rem" }}>
-                      <li>
-                        Ha förmågan att förstå kundens affär och koppling till
-                        tekniken samt diskutera det med kund och kollegor
-                      </li>
-                      <li>God samarbetsförmåga</li>
-                      <li>
-                        - Goda kunskaper inom vår primära tech-stack Open Source
-                        (ex Node.js, React, Docker/Kubernetes)
-                      </li>
-                      <li>
-                        Skriva kod som är testbar samt skriva automatiska tester
-                      </li>
-                      <li>
-                        Erfarenhet av att utvärdera nya teknik såsom språk,
-                        frameworks
-                      </li>
-                      <li>5+ års erfarenhet av utveckling</li>
-                      <li>
-                        3+ års erfarenhet av ledarskap (facilitering, leda
-                        grupper, hålla utbildning, kultur)
-                      </li>
-                      <li>Erfarenhet av agil, värdedriven utveckling</li>
-                      <li>Erfarenhet av konsultverksamhet</li>
-                      <li>Behärska svenska och engelska i tal och skrift</li>
-                    </ul>
-                  </Text>
+                  <ul style={{ listStyle: "disc", marginLeft: "3rem" }}>
+                    <li>
+                      Ha förmågan att förstå kundens affär och koppling till
+                      tekniken samt diskutera det med kund och kollegor
+                    </li>
+                    <li>God samarbetsförmåga</li>
+                    <li>
+                      Goda kunskaper inom vår primära tech-stack
+                      (t.ex. Node.js, React, Docker/Kubernetes)
+                    </li>
+                    <li>
+                      Skriva kod som är testbar samt skriva automatiska tester
+                    </li>
+                    <li>
+                      Erfarenhet av att utvärdera nya teknik såsom språk,
+                      frameworks
+                    </li>
+                    <li>5+ års erfarenhet av utveckling</li>
+                    <li>
+                      3+ års erfarenhet av ledarskap (t.ex. facilitering, leda grupper, hålla utbildning, kultur, tech lead)
+                    </li>
+                    <li>Erfarenhet av agil, värdedriven utveckling</li>
+                    <li>Erfarenhet av konsultverksamhet</li>
+                    <li>Behärska svenska och engelska i tal och skrift</li>
+                  </ul>
                 </Box>
                 <Box pb={["40px", null, "65px"]}>
                   <Title variant="card" mb="18px">
-                    Meriterande
+                    Plus i kanten
                   </Title>
-                  <Text variant="small">
-                    <ul style={{ listStyle: "disc", marginLeft: "3rem" }}>
-                      <li>
-                        Du är aktiv och bidrar i utvecklarcommunityn (t.ex. Open
-                        Source, Meetups)
-                      </li>
-                      <li>Kunskaper i hypotesdriven utveckling</li>
-                      <li>Erfarenhet av försäljning</li>
-                      <li>Erfarenhet av rekrytering</li>
-                      <li>
-                        Erfarenhet av andra språk (t.ex. Elixir, Go, Python,
-                        .NET)
-                      </li>
-                      <li>E2E-testning med t.ex. Cypress eller Selenium</li>
-                      <li>Förståelse för UX-design</li>
-                      <li>Erfarenhet som agil coach eller scrum master</li>
-                    </ul>
-                  </Text>
+                  <ul style={{ listStyle: "disc", marginLeft: "3rem" }}>
+                    <li>
+                      Du är aktiv och bidrar i utvecklarcommunityn (t.ex. Open
+                      Source, Meetups)
+                    </li>
+                    <li>
+                      Goda kunskaper i något annat språk (t.ex. Elixir, Go, Python, .NET, Rust, Java)
+                    </li>
+                    <li>Erfarenhet av hypotesdriven utveckling</li>
+                    <li>Erfarenhet av försäljning</li>
+                    <li>Erfarenhet av rekrytering</li>
+                    <li>Förståelse för UX-design</li>
+                    <li>Erfarenhet som agil coach eller scrum master</li>
+                    <li>
+                      Kanske kan du något vi inte ens visste att vi behövde? Överraska oss!
+                    </li>
+                  </ul>
                 </Box>
                 <Box pb={["40px", null, "65px"]}>
                   <Title variant="card" mb="18px">
-                    Övrigt
+                    Ansökan
                   </Title>
                   <Text variant="small">
-                    Omfattning: Heltid
+                    Anställningsform: Tillsvidareanställning. 6 månaders provanställning, sedan fast anställning
                     <br />
-                    <br />
-                    Tillsättning: Snarast
-                    <br />
+                    Start: Omgående
                     <br />
                     Plats: Göteborg eller Stockholm
                     <br />
-                    <br />
-                    Frågor: Till{" "}
+                    Ansök genom att maila ditt CV & några rader om dig till{" "}
+                    <a href="mailto:joinus@iteam.se">
+                      joinus@iteam.se
+                    </a>
+                  </Text>
+                </Box>
+                <Box pb={["40px", null, "65px"]}>
+                  <Title variant="card" mb="18px">
+                    Undrar du något om tjänsten?
+                  </Title>
+                  <Text variant="small">
+                    Skicka din fråga till{" "}
                     <a href="mailto:johanna.grahn@iteam.se">
                       johanna.grahn@iteam.se
-                    </a>
+                    </a> (Head of People and Culture)
+                    eller{" "}
+                    <a href="mailto:christian.landgren@iteam.se">
+                      christian.landgren@iteam.se
+                    </a> (CEO)
                   </Text>
                 </Box>
               </Col>
