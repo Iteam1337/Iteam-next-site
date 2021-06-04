daf5e229c26fdecaec7b363ba242010161e9a910
Rebecca Norén <rebecca.noren@iteam.se>
2021-06-04 11:08:40 +0200

    chore: update texte

--- src/sections/about/Content.js
+++ src/sections/about/Content.js
@@ -4,7 +4,7 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import { Title, Section, Box, Text } from "../../components/Core";
 
-import imgContent1 from "../../assets/image/jpeg/Daily-Vimla-Turing-1-Lowres.jpg";
+import imgContent1 from "../../assets/image/jpeg/_DSC6527_.jpg";
 import imgContent2 from "../../assets/image/jpeg/Lounge-Martin-Mikael-Christian-Lowres.jpg";
 
 const ContentImg = styled(Box)`
@@ -12,6 +12,7 @@ const ContentImg = styled(Box)`
   border-radius: 10px;
   overflow: hidden;
   max-height: 515px;
+  height: 100%;
 `;
 
 const Content = () => (
