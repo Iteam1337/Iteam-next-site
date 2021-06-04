0362a78b47a459a967bed9fed854b1df872893da
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-06-04 14:34:42 +0200

    shorten hero text

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,11 +12,11 @@ const About = () => {
     <>
       <PageWrapper>
         <Hero title="Skapa värde, ha kul, göra något bra">
-          Det är våra värderingar. De lever vi efter varje dag. <br/>
-          Iteam startades 1995 med drömmen att förändra samhället till något bättre med teknik. 
+          Det är våra värderingar. De lever vi efter varje dag. <br />
+          {/* Iteam startades 1995 med drömmen att förändra samhället till något bättre med teknik. 
           Det har vi hållit på med sedan dess.
           För oss betyder det att inte bygga appar för apparnas skull. Vi fixar inte problem som inte behöver fixas. 
-          Däremot är världen full av problem som vi kan lösa. Ibland handlar det om att underlätta vardagen för småbarnsföräldrar, ibland om att förändra globala logistiksystem.
+          Däremot är världen full av problem som vi kan lösa. Ibland handlar det om att underlätta vardagen för småbarnsföräldrar, ibland om att förändra globala logistiksystem. */}
         </Hero>
         <Content />
         <Team />
