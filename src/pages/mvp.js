47115826455bd4d17fb830a460d8e8c007ef9e6a
Emil Ahlbäck <emil.ahlback@iteam.se>
2021-06-04 12:39:36 +0200

    chore: Render start page's Testimonial on Iteam MVP

--- src/pages/mvp.js
+++ src/pages/mvp.js
@@ -5,7 +5,8 @@ import Feature1 from "../sections/mvp/Feature1";
 import Content1 from "../sections/mvp/Content1";
 import Feature2 from "../sections/mvp/Feature2";
 import Content2 from "../sections/mvp/Content2";
-import Testimonial from "../sections/mvp/Testimonial";
+// import Testimonial from "../sections/mvp/Testimonial";
+import Testimonial from "../sections/startpage/Testimonial";
 import CTA from "../sections/mvp/CTA";
 
 const LandingPage2 = () => {
