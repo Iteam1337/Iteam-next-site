6096b343a21c4f3f1f59ec6cb76ce9459e67ff56
Izabella Larsson <izzwii.izabella@gmail.com>
2021-06-09 16:02:34 +0200

    chore: no repeat on hero image (#51)

--- src/sections/karriar/Hero.js
+++ src/sections/karriar/Hero.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 import { Container, Row, Col } from "react-bootstrap";
 
 import { Title, Section, Text } from "../../components/Core";
-import imgBg from '../../assets/image/jpeg/career-hero.jpeg'
+import imgBg from "../../assets/image/jpeg/career-hero.jpeg";
 
 const SectionStyled = styled(Section)`
-
-  background-image: linear-gradient(to left, rgba(0, 0, 0, 0.127), rgba(0, 0, 0, 0.8)), 
-                    url(${imgBg});
+  background: linear-gradient(to left, rgba(0, 0, 0, 0.127), rgba(0, 0, 0, 0.8)),
+    url(${imgBg}) no-repeat center;
+  background-size: cover;
 `;
 
 const Hero = ({ title, children, ...rest }) => {
@@ -20,14 +20,18 @@ const Hero = ({ title, children, ...rest }) => {
         bg="dark"
         // {...rest}
       >
-        <Container css={`
+        <Container
+          css={`
             z-index: 10;
             margin-left: 0px;
-          `}>
+          `}
+        >
           <Row>
             <Col lg="6">
               <div>
-                <Title color="light" variant="hero">{title}</Title>
+                <Title color="light" variant="hero">
+                  {title}
+                </Title>
                 <Text color="light">{children}</Text>
               </div>
             </Col>
