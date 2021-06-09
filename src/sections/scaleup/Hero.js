5d380e15ccc842ab7fd5bd634d915c7b8cbb5b36
izabellalarsson <izzwii.izabella@gmail.com>
2021-06-09 13:57:13 +0200

    chore: use less gradient

--- src/sections/scaleup/Hero.js
+++ src/sections/scaleup/Hero.js
@@ -19,17 +19,15 @@ import imgL2HeroBg from "../../assets/image/jpeg/Hans-Radu-Martin-Office-Lowres.
 
 const SectionStyled = styled(Section)`
   background: 
-  /* top, transparent black, faked with gradient */ 
-  linear-gradient(
-    to right,
-    rgba(255, 255, 255, 0.9), 
-    rgba(255, 255, 255, 0.4)
-  ),
-  /* bottom, image */
-  url(${imgL2HeroBg}) no-repeat center 20%;
+  /* top, transparent black, faked with gradient */ linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0.9),
+      rgba(255, 255, 255, 0.1),
+      rgba(255, 255, 255, 0.1)
+    ),
+    /* bottom, image */ url(${imgL2HeroBg}) no-repeat center 20%;
   background-size: cover;
   padding-top: 128px;
-
 
   @media ${device.lg} {
     background-position: center;
@@ -85,11 +83,11 @@ const Hero = () => {
               data-aos-duration="300"
               data-aos-delay="500"
               data-aos-once="true"
-            > Iteam Scale-Up – När ditt team behöver extra kraft
+            >
+              {" "}
+              Iteam Scale-Up – När ditt team behöver extra kraft
               <Box py={[null, null, null, 4]} pr={5} pt={[4, null]}>
-                <Title variant="hero">
-                Ibland behöver man förstärkning
-                </Title>
+                <Title variant="hero">Ibland behöver man förstärkning</Title>
                 {/* <Text mb={4}>
                 Oavsett om du har ett problem du behöver lösa eller redan har en idé så hjälper vi dig. Vi börjar där du står och utgår ifrån det helt enkelt. Ibland gör vi en <a href="/designsprint">Designsprint</a> för att kickstarta samarbetet. Andra gånger gör vi research om användarbehov för att försäkra oss om att vi löser rätt problem.
                 </Text> */}
