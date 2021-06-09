dcd292a3c5d6d1e54350ec75223c97faaf75590f
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-06-09 15:14:27 +0200

    chore: removed google tag manager

--- src/components/GoogleAnalytics/GoogleAnalytics.js
+++ src/components/GoogleAnalytics/GoogleAnalytics.js
@@ -10,11 +10,6 @@ export default class extends Component {
             <script
               async
               defer
-              src="https://www.googletagmanager.com/gtag/js?id=UA-2430046-1"
-            ></script>
-            <script
-              async
-              defer
               dangerouslySetInnerHTML={{
                 __html: `window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
