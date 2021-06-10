3c9018d7ab7072edcc1bbf7351ac2bb1df886992
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-06-10 10:31:53 +0200

    removed unused images

--- src/sections/about/Team.js
+++ src/sections/about/Team.js
@@ -5,14 +5,7 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import { Title, Section, Box, Text } from "../../components/Core";
 
-import imgU1 from "../../assets/image/jpeg/about-team-1.jpg";
-import imgU2 from "../../assets/image/jpeg/about-team-3.jpg";
-import imgU3 from "../../assets/image/jpeg/about-team-4.jpg";
-import imgU4 from "../../assets/image/jpeg/about-team-5.jpg";
-import imgU5 from "../../assets/image/jpeg/about-team-6.jpg";
-import imgU6 from "../../assets/image/jpeg/about-team-2.jpg";
-
-import team from './team.json'
+import team from "./team.json";
 
 const CardImage = styled.div`
   max-width: 160px;
@@ -36,11 +29,7 @@ const TeamCard = ({ email, name, title, status, phoneNumber, ...rest }) => (
     {...rest}
   >
     <CardImage>
-      <Gravatar
-        email={email}
-        className="img-fluid"
-        size={200}
-        />
+      <Gravatar email={email} className="img-fluid" size={200} />
     </CardImage>
     <div className="text-center">
       <Title variant="card" fontSize="24px" letterSpacing={-0.75} my={1}>
@@ -80,7 +69,8 @@ const Team = () => (
             <div id="medarbetare" className="">
               <Title>Medarbetare</Title>
               <Text>
-                Här hittar du oss som jobbar på Iteam, tveka inte att höra av dig!
+                Här hittar du oss som jobbar på Iteam, tveka inte att höra av
+                dig!
               </Text>
             </div>
           </Col>
@@ -88,11 +78,17 @@ const Team = () => (
         <Row className="align-items-center justify-content-center">
           {team
             .sort((a, b) => a.fullname > b.fullname)
-            .map(({fullname, title, email, phoneNumber, status}) => 
+            .map(({ fullname, title, email, phoneNumber, status }) => (
               <Col sm="6" md="5" lg="4" className="mt-3 mt-lg-4" key={email}>
-                <TeamCard email={email} name={fullname} title={title} status={status} phoneNumber={phoneNumber} />
+                <TeamCard
+                  email={email}
+                  name={fullname}
+                  title={title}
+                  status={status}
+                  phoneNumber={phoneNumber}
+                />
               </Col>
-          )}
+            ))}
         </Row>
       </Container>
     </Section>
