022cbe446ac077ee9611047d7add2ac4cae496c7
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-06-11 13:48:28 +0200

    add cross on hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -262,6 +262,13 @@ const MenuDropdown = styled.ul`
   }
 `;
 
+const CrossWrapper = styled.div`
+    position: absolute;
+    top: 11px;
+    right: 24px;
+}
+`
+
 const Header = ({ isDark = false }) => {
   const gContext = useContext(GlobalContext);
   const [showScrolling, setShowScrolling] = useState(false);
@@ -337,6 +344,7 @@ const Header = ({ isDark = false }) => {
                   className="navbar-nav d-none d-lg-flex"
                   dark={isDark ? 1 : 0}
                 >
+
                   {menuItems.map(
                     (
                       { label, isExternal = false, name, items, ...rest },
@@ -479,7 +487,9 @@ const Header = ({ isDark = false }) => {
               onClick={toggleMenu}
               dark={isDark ? 1 : 0}
             >
-              <i className="icon icon-menu-34 icon-burger d-block fa-2x"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill={isDark ? "white" : "black"} class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+              </svg>
             </ToggleButton>
           </nav>
         </Container>
@@ -488,6 +498,14 @@ const Header = ({ isDark = false }) => {
         show={gContext.visibleOffCanvas}
         onHideOffcanvas={gContext.toggleOffCanvas}
       >
+        <CrossWrapper onClick={() => {
+          document.body.style.overflow = "visible";
+          gContext.toggleOffCanvas()
+        }}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="white" class="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        </CrossWrapper>
         <NestedMenu menuItems={menuItems} />
       </Offcanvas>
     </>
