f5dc42076588589ba4cfcca4027ea5ab79890056
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-06-16 13:51:45 +0200

    update class to className

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -487,7 +487,7 @@ const Header = ({ isDark = false }) => {
               onClick={toggleMenu}
               dark={isDark ? 1 : 0}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill={isDark ? "white" : "black"} class="bi bi-list" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill={isDark ? "white" : "black"} className="bi bi-list" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
               </svg>
             </ToggleButton>
@@ -502,7 +502,7 @@ const Header = ({ isDark = false }) => {
           document.body.style.overflow = "visible"
           gContext.toggleOffCanvas()
         }}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="white" class="bi bi-x" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="white" className="bi bi-x" viewBox="0 0 16 16">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
           </svg>
         </CrossWrapper>
