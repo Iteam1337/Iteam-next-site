8f22ef684c2518b6a90263f7df5d371e9959c338
Mikaela <54809602+mikaelaalu@users.noreply.github.com>
2021-06-17 11:28:04 +0200

    fixed styled li item (#74)

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
+import styled from "styled-components"
 
 import PageWrapper from "../components/PageWrapper"
 import { Section, Title, Text, Box, List, Anchor } from "../components/Core"
+
+const StyledLi = styled.li`
+font-weight: 100 !important;
+`
 
 const Privacy = () => {
   return (
@@ -44,11 +49,11 @@ const Privacy = () => {
                     Vi samlar in personuppgifter på följande sätt:
                   </Text>
                   <List>
-                    <li>Du själv lämnar dessa till oss, exempelvis i samband med att du blir kund hos oss.</li>
-                    <li>Du själv lämnar dessa till oss i samband med frivilliga användartester, workshops eller liknande tillställningar.</li>
-                    <li>Du själv lämnar dessa till oss, exempelvis i samband med att registrerar dig till våra event, i kontakt försäljning, i kontakt via e-post, brev eller via kontaktformulär.</li>
-                    <li>Du själv lämnar dina uppgifter till en leverantör och samarbetspartners som i sin tur har ditt medgivande att dela dina personuppgifter med oss.</li>
-                    <li>Vi förbehåller oss också rätten att inhämta uppgifter från tredje part (ex Creditsafe, SPAR, UC m.fl.) Dessa uppgifter är exempelvis namn, kontaktuppgifter, bolagsengagemang och demografisk data, kreditvärdighet.</li>
+                    <StyledLi>Du själv lämnar dessa till oss, exempelvis i samband med att du blir kund hos oss.</StyledLi>
+                    <StyledLi>Du själv lämnar dessa till oss i samband med frivilliga användartester, workshops eller liknande tillställningar.</StyledLi>
+                    <StyledLi>Du själv lämnar dessa till oss, exempelvis i samband med att registrerar dig till våra event, i kontakt försäljning, i kontakt via e-post, brev eller via kontaktformulär.</StyledLi>
+                    <StyledLi>Du själv lämnar dina uppgifter till en leverantör och samarbetspartners som i sin tur har ditt medgivande att dela dina personuppgifter med oss.</StyledLi>
+                    <StyledLi>Vi förbehåller oss också rätten att inhämta uppgifter från tredje part (ex Creditsafe, SPAR, UC m.fl.) Dessa uppgifter är exempelvis namn, kontaktuppgifter, bolagsengagemang och demografisk data, kreditvärdighet.</StyledLi>
                   </List>
                 </Box>
                 <Box mt={["40px", null, "50px"]}>
@@ -59,18 +64,18 @@ const Privacy = () => {
                     De kategorier av personuppgifter som vi samlar, eller har rätt att samla in, är:
                   </Text>
                   <List>
-                    <li>
+                    <StyledLi>
                       Kundinformation, det vill säga de uppgifter som behövs för att kunna bli kund hos oss
-                    </li>
-                    <li>
+                    </StyledLi>
+                    <StyledLi>
                       Kontaktuppgifter i samband med event, användartester, paneler, eller motsvarande
-                    </li>
-                    <li>
+                    </StyledLi>
+                    <StyledLi>
                       Information om köpta produkter/tjänster och betalningshistorik
-                    </li>
-                    <li>
+                    </StyledLi>
+                    <StyledLi>
                       Besöksinformation på icke-inloggade och inloggade tjänster, det vill säga din användargenererade data, data rörande vilken enhet som används och viss interaktionsdata
-                    </li>
+                    </StyledLi>
                   </List>
                 </Box>
                 <Box mt={["40px", null, "50px"]}>
@@ -81,15 +86,15 @@ const Privacy = () => {
                   <Text style={{ marginBottom: '1rem' }} variant="small">
                     Vi samlar bara in personuppgifter för ändamål som har stöd i gällande
                     dataskyddsförordning. Det vanligaste är att uppgifterna samlas in för
-                    behandling med stöd av den rättsliga grunden <b>fullgörande av avtal.</b>
+                    behandling med stöd av den rättsliga grunden <b>fullgörande av avtal.</b> {" "}
                     Ändamålen är till exempel:
                   </Text>
 
                   <List>
-                    <li>ingå ett avtal med oss</li>
-                    <li>få en offert på ett erbjudande</li>
-                    <li>köpa en produkt/tjänst</li>
-                    <li>fakturera tjänster</li>
+                    <StyledLi>  ingå ett avtal med oss</StyledLi>
+                    <StyledLi>  få en offert på ett erbjudande</StyledLi>
+                    <StyledLi>  köpa en produkt/tjänst</StyledLi>
+                    <StyledLi>  fakturera tjänster</StyledLi>
                   </List>
                   <Text style={{ marginBottom: '1rem' }} variant="small">
 
@@ -97,17 +102,19 @@ const Privacy = () => {
                     flera ändamål, till exempel:
                   </Text>
                   <List>
-                    <li> för marknadsföring och profilering, det vill säga för att kommunicera relevanta erbjudanden till dig </li>
-                    <li> för försäljning av våra erbjudanden</li>
-                    <li>  för att skicka nyhetsbrev</li>
-                    <li> för att utveckla och förbättra våra produkter och tjänster </li>
-                    <li> för undersökningar och utvärderingar </li>
+                    <StyledLi>   för marknadsföring och profilering, det vill säga för att kommunicera relevanta erbjudanden till dig </StyledLi>
+                    <StyledLi>   för försäljning av våra erbjudanden</StyledLi>
+                    <StyledLi>    för att skicka nyhetsbrev</StyledLi>
+                    <StyledLi>   för att utveckla och förbättra våra produkter och tjänster </StyledLi>
+                    <StyledLi>   för undersökningar och utvärderingar </StyledLi>
                   </List>
+                  <Text variant="small">
 
-                  Du kan även <b> lämna samtycke </b> till behandling i vissa fall då inget
-                  av ovanstående gäller. Ett lämnat samtycke är en aktiv handling från dig
-                  och den kan återkallas vilket leder till att vi inte längre behandlar dina
-                  personuppgifter ytterligare för det ändamålet.
+                    Du kan även <b> lämna samtycke </b> till behandling i vissa fall då inget
+                    av ovanstående gäller. Ett lämnat samtycke är en aktiv handling från dig
+                    och den kan återkallas vilket leder till att vi inte längre behandlar dina
+                    personuppgifter ytterligare för det ändamålet.
+                  </Text>
 
                 </Box>
                 <Box mt={["40px", null, "50px"]}>
@@ -121,12 +128,12 @@ const Privacy = () => {
                   </Text>
 
                   <List>
-                    <li>  Ekonomiservice-leverantörer</li>
-                    <li>  IT-leverantörer</li>
-                    <li>  Säljpartners</li>
-                    <li>  CRM-leverantörer</li>
-                    <li>  Mediebyrå- och reklambyrå-partners</li>
-                    <li>  Uppdragsgivare</li>
+                    <StyledLi>    Ekonomiservice-leverantörer</StyledLi>
+                    <StyledLi>    IT-leverantörer</StyledLi>
+                    <StyledLi>    Säljpartners</StyledLi>
+                    <StyledLi>    CRM-leverantörer</StyledLi>
+                    <StyledLi>    Mediebyrå- och reklambyrå-partners</StyledLi>
+                    <StyledLi>    Uppdragsgivare</StyledLi>
                   </List>
 
                 </Box>
@@ -176,9 +183,9 @@ const Privacy = () => {
                     Du har rätt till tillgång, rättelse och radering av uppgifter enligt följande:
                   </Text>
                   <List>
-                    <li>Du har rätt att få information om i vilka personuppgifter vi innehar om dig.</li>
-                    <li>Du har rätt att begära rättelse av dina personuppgifter om information om dom är felaktiga.</li>
-                    <li>Du har rätt att få dina personuppgifter raderade om behandlingen grundar sig enbart på ditt samtycke och du återkallar samtycket, om uppgifterna inte längre behövs för de ändamål som de behandlats för eller om radering krävs för att uppfylla en rättslig skyldighet.</li>
+                    <StyledLi>  Du har rätt att få information om i vilka personuppgifter vi innehar om dig.</StyledLi>
+                    <StyledLi>  Du har rätt att begära rättelse av dina personuppgifter om information om dom är felaktiga.</StyledLi>
+                    <StyledLi>  Du har rätt att få dina personuppgifter raderade om behandlingen grundar sig enbart på ditt samtycke och du återkallar samtycket, om uppgifterna inte längre behövs för de ändamål som de behandlats för eller om radering krävs för att uppfylla en rättslig skyldighet</StyledLi>
                   </List>
                 </Box>
 
