d46d0c7d181ac680061f1d0cad1a935b5e85c3f6
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-06-18 16:06:27 +0200

    removed import for googla analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,6 @@ import Pricing from "../sections/startpage/Pricing"
 import Faq from "../sections/startpage/Faq"
 import CTA from "../sections/startpage/CTA"
 import PageWrapper from "../components/PageWrapper"
-import GoogleAnalytics from "../components/GoogleAnalytics/GoogleAnalytics"
 
 const LandingPage4 = () => {
   return (
