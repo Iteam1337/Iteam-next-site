775382f767961df46ceb723b646073d875204711
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-09-16 17:03:10 +0200

    chore: add icons to deskStructure

--- studio/src/deskStructure.js
+++ studio/src/deskStructure.js
@@ -1,4 +1,5 @@
 import S from '@sanity/desk-tool/structure-builder';
+import { FaRegFileAlt, FaRegFolderOpen, FaRegFileImage, FaRegClone } from 'react-icons/fa'
 
 export default () =>
     S.list()
@@ -6,24 +7,28 @@ export default () =>
         .items([
             S.listItem()
                 .title('Sidor')
+                .icon(FaRegClone)
                 .child(
                     S.list()
                         .title('Sidor')
                         .items([
                             S.listItem()
                                 .title('Case')
+                                .icon(FaRegFileImage)
                                 .child(
                                     S.list()
                                         .title('Case')
                                         .items([
                                             S.listItem()
                                                 .title('Case landningssida')
+                                                .icon(FaRegFileAlt)
                                                 .child(
                                                     S.document().schemaType('casePage').documentId('casePage')
                                                 ),
                                             S.divider(),
                                             S.listItem()
                                                 .title('Case')
+                                                .icon(FaRegFolderOpen)
                                                 .child(
                                                     S.documentTypeList('casePost').title('Alla case')
                                                 )
@@ -32,6 +37,7 @@ export default () =>
                             S.divider(),
                             S.listItem()
                                 .title('Startsidan')
+                                .icon(FaRegFileImage)
                                 .child(
                                     S.document()
                                         .title('Startsidan')
@@ -44,6 +50,6 @@ export default () =>
             // We also need to remove the new singletons from the main list
             ...S.documentTypeListItems().filter(
                 (listItem) =>
-                    !['casePage', 'casePost', 'startPage', 'hero', 'cta', 'imageCard', 'mediaTypes', 'titleWithCTA'].includes(listItem.getId())
+                    !['casePage', 'casePost', 'startPage', 'hero', 'cta', 'imageCard', 'mediaTypes', 'titleWithCTA', 'blockText'].includes(listItem.getId())
             ),
         ]);
