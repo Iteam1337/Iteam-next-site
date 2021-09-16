c4a7e9f4a717ea68166f1a7f7d4b6ec76f6ab102
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-09-16 17:56:46 +0200

    add newsPage and newsPost

--- studio/schemas/pages/casePage.js
+++ studio/schemas/pages/casePage.js
@@ -4,7 +4,7 @@ export default {
     title: 'Case',
     fields: [
         { type: 'title', name: 'title', title: 'Rubrik' },
-        { type: 'string', name: 'subtitle', title: 'Underrubrik', validation: (Rules) => Rules.required().error('Du behöver ange en rubrik') },
+        { type: 'string', name: 'subtitle', title: 'Underrubrik' },
         { type: 'imageCard', name: 'imageCardOne', title: 'Kort' },
         { type: 'imageCard', name: 'imageCardTwo', title: 'Kort' },
         { type: 'titleWithCTA', name: 'titleWithCTA', title: 'Text med CTA knapp', }
--- studio/src/deskStructure.js
+++ studio/src/deskStructure.js
@@ -1,76 +1,105 @@
 import S from '@sanity/desk-tool/structure-builder';
 import {
-  FaRegFileAlt,
-  FaRegFolderOpen,
-  FaRegFileImage,
-  FaRegClone,
+    FaRegFileAlt,
+    FaRegFolderOpen,
+    FaRegFileImage,
+    FaRegClone,
 } from 'react-icons/fa';
 
 export default () =>
-  S.list()
-    .title('Innehåll')
-    .items([
-      S.listItem()
-        .title('Sidor')
-        .icon(FaRegClone)
-        .child(
-          S.list()
-            .title('Sidor')
-            .items([
-              S.listItem()
-                .title('Case')
-                .icon(FaRegFileImage)
+    S.list()
+        .title('Innehåll')
+        .items([
+            S.listItem()
+                .title('Sidor')
+                .icon(FaRegClone)
                 .child(
-                  S.list()
-                    .title('Case')
-                    .items([
-                      S.listItem()
-                        .title('Case landningssida')
-                        .icon(FaRegFileAlt)
-                        .child(
-                          S.document()
-                            .schemaType('casePage')
-                            .documentId('casePage')
-                        ),
-                      S.divider(),
-                      S.listItem()
-                        .title('Case')
-                        .icon(FaRegFolderOpen)
-                        .child(
-                          S.documentTypeList('casePost').title('Alla case')
-                        ),
-                    ])
-                ),
-              S.divider(),
-              S.listItem()
-                .title('Startsidan')
-                .icon(FaRegFileImage)
-                .child(
-                  S.document()
-                    .title('Startsidan')
-                    .schemaType('startPage')
-                    .documentId('startPage')
-                ),
-            ])
-        ),
+                    S.list()
+                        .title('Sidor')
+                        .items([
+                            S.listItem()
+                                .title('Case')
+                                .icon(FaRegFileImage)
+                                .child(
+                                    S.list()
+                                        .title('Case')
+                                        .items([
+                                            S.listItem()
+                                                .title('Case landningssida')
+                                                .icon(FaRegFileAlt)
+                                                .child(
+                                                    S.document()
+                                                        .schemaType('casePage')
+                                                        .documentId('casePage')
+                                                        .title('Case landningssida')
+                                                ),
+                                            S.divider(),
+                                            S.listItem()
+                                                .title('Case')
+                                                .icon(FaRegFolderOpen)
+                                                .child(
+                                                    S.documentTypeList('casePost').title('Alla case')
+                                                ),
+                                        ])
+                                ),
+                            S.divider(),
+                            S.listItem()
+                                .title('Aktuellt')
+                                .icon(FaRegFileImage)
+                                .child(
+                                    S.list()
+                                        .title('Aktuellt')
+                                        .items([
+                                            S.listItem()
+                                                .title('Aktuellt landningssida')
+                                                .icon(FaRegFileAlt)
+                                                .child(
+                                                    S.document()
+                                                        .schemaType('newsPage')
+                                                        .documentId('newsPage')
+                                                        .title('Akuellt landningssida')
+                                                ),
+                                            S.divider(),
+                                            S.listItem()
+                                                .title('Nyheter')
+                                                .icon(FaRegFolderOpen)
+                                                .child(
+                                                    S.documentTypeList('newsPost').title('Alla nyheter')
+                                                ),
+                                        ])
+                                ),
 
-      // We also need to remove the new singletons from the main list
-      ...S.documentTypeListItems().filter(
-        (listItem) =>
-          ![
-            'casePage',
-            'casePost',
-            'startPage',
-            'hero',
-            'cta',
-            'imageCard',
-            'mediaTypes',
-            'titleWithCTA',
-            'blockText',
-            'textWithImage',
-            'textGrid',
-            'carousel',
-            'carouselContent',
-          ].includes(listItem.getId())
-      ),
-    ]);
+
+                            S.divider(),
+                            S.listItem()
+                                .title('Startsidan')
+                                .icon(FaRegFileImage)
+                                .child(
+                                    S.document()
+                                        .title('Startsidan')
+                                        .schemaType('startPage')
+                                        .documentId('startPage')
+                                ),
+                        ])
+                ),
+
+            // We also need to remove the new singletons from the main list
+            ...S.documentTypeListItems().filter(
+                (listItem) =>
+                    ![
+                        'casePage',
+                        'casePost',
+                        'startPage',
+                        'hero',
+                        'cta',
+                        'imageCard',
+                        'mediaTypes',
+                        'titleWithCTA',
+                        'blockText',
+                        'textWithImage',
+                        'textGrid',
+                        'carousel',
+                        'carouselContent',
+                    ].includes(listItem.getId())
+            ),
+        ]);
