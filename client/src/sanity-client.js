54dc05272fcdee21e1d023b28d49bf689ef5021e
izabellalarsson <izzwii.izabella@gmail.com>
2021-09-29 11:50:47 +0200

    chore: update sanity client file

--- client/src/sanity-client.js
+++ client/src/sanity-client.js
@@ -3,7 +3,4 @@ import sanityClient from "@sanity/client"
 export default sanityClient({
   projectId: "3gob9bjq",
   dataset: "production",
-  useCdn: true,
-  token: process.env.SANITY_READ_DATA_TOKEN,
-  watchMode: true,
 })
