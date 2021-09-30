3c57de6ea3fcc85804248ebc821b46b74a1ad066
izabellalarsson <izzwii.izabella@gmail.com>
2021-09-30 11:15:36 +0200

    chore: use development dataset locally

--- client/src/sanity-client.js
+++ client/src/sanity-client.js
@@ -2,6 +2,6 @@ import sanityClient from "@sanity/client"
 
 export default sanityClient({
   projectId: "3gob9bjq",
-  dataset: "production",
+  dataset: process.env.NODE_ENV,
   token: process.env.SANITY_READ_DATA_TOKEN,
 })
