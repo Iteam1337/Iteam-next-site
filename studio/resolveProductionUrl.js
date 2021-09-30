234152e01d54961b9d060d04acddeab6edddc2c3
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-09-30 16:30:05 +0200

    chore: live preview for news post

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -8,10 +8,10 @@ export default function resolveProductionUrl(doc) {
     const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
 
     switch (doc._type) {
-        // case 'newsPost':
-        //     return `${baseUrl}/aktuellt/${doc.slug.current}`
-        // case 'coworker':
-        //     return `${baseUrl}/medarbetare/${doc.slug.current}`
+        case 'newsPost':
+            return `${baseUrl}/aktuellt/${doc.slug.current}`
+        case 'coworker':
+            return `${baseUrl}/medarbetare/${doc.slug.current}`
         case 'casePost':
             return `${baseUrl}/case/${doc.slug.current}`
         default:
