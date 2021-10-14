4d64fd72a9810151ddb862c7d242f0462a4a793b
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-10-14 12:03:59 +0200

    chore: metaTags and preview are required fields

--- studio/schemas/shared/metaTags.js
+++ studio/schemas/shared/metaTags.js
@@ -2,6 +2,8 @@ export default {
   type: 'document',
   name: 'metaTags',
   title: 'Meta tags',
+  validation: (Rules) =>
+    Rules.required().error('Du behöver lägga in meta tags'),
   fields: [
     {
       type: 'title',
@@ -23,4 +25,4 @@ export default {
         Rules.required().error('Lägg in en bild som visas som meta data.'),
     },
   ],
-};
+}
