5a0e8c5ccd822360bcd2b44dc1928f16a02ab014
Izabella Larsson <izzwii.izabella@gmail.com>
2021-10-15 16:34:13 +0200

    fix: live preview about (#129)
    * fix: live preview /about
    
    * chore: clean up
    
    * chore: get static from coworker schema
    
    * rfeat: rename file
    
    * chore: get all coworkers
    
    * fix: don't destruct data

---
+++ client/src/pages/about/[slug].js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PageWrapper from '../../components/PageWrapper'
+import Medarbetare from '../../sections/about/Medarbetare'
+import Hero from '../../sections/about/CoworkerHero'
+import { groq } from 'next-sanity'
+import { usePreviewSubscription } from '../../lib/sanity'
+import { getClient } from '../../lib/sanity.server'
+import { filterDataToSingleItem } from '../../utils/helpers'
+import ExitPreviewButton from '../../components/ExitPreviewButton'
+
+const CoworkerPage = ({ data, preview = false }) => {
+  const { data: previewData } = usePreviewSubscription(data?.coworkerQuery, {
+    params: data?.queryParams ?? {},
+    initialData: data?.post,
+    enabled: preview,
+  })
+  const post = filterDataToSingleItem(previewData, preview)
+
+  return (
+    <PageWrapper footerDark>
+      {preview && <ExitPreviewButton />}
+      {post && (
+        <>
+          <Hero
+            title={post?.fullname ?? post.fullname}
+            heroImage={post?.heroImage ?? post.heroImage}
+          >
+            {post?.role ?? post.role}
+          </Hero>
+          <Medarbetare info={post ?? post} />
+        </>
+      )}
+    </PageWrapper>
+  )
+}
+
+const coworkerQuery = groq`
+*[_type == 'coworker' && slug.current == $slug] {
+  ...,
+ }
+`
+
+export async function getStaticProps({ params, preview = false }) {
+  const queryParams = { slug: params.slug }
+  const data = await getClient(preview).fetch(coworkerQuery, queryParams)
+
+  if (!data) return { notFound: true }
+
+  const post = filterDataToSingleItem(data, preview)
+
+  return {
+    props: {
+      preview,
+      data: { post, coworkerQuery, queryParams },
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const query = groq`*[_type == 'coworker' && defined(slug.current)][].slug.current`
+  const pages = await getClient().fetch(query)
+  return {
+    paths: pages.map((slug) => `/about/${slug}`),
+    fallback: true,
+  }
+}
+
+export default CoworkerPage
