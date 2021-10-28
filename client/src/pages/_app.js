886bdf198a973f2924f43a66a3db49494baacd73
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-10-28 16:52:33 +0200

    chore: fetch footer content from sanity

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,29 +1,38 @@
+import React, { useEffect } from 'react'
 // import App from 'next/app'
-import TagManager from "react-gtm-module"
-import Layout from "../components/Layout"
-import { GlobalProvider } from "../context/GlobalContext"
+import TagManager from 'react-gtm-module'
+import Layout from '../components/Layout'
+import { GlobalProvider } from '../context/GlobalContext'
+import { getClient } from '../lib/sanity.server'
+import { groq } from 'next-sanity'
 
-import "../components/Layout/bootstrap-custom.scss"
-import "../../node_modules/slick-carousel/slick/slick.css"
-import "../../node_modules/slick-carousel/slick/slick-theme.css"
-import "../../node_modules/aos/dist/aos.css"
+import '../components/Layout/bootstrap-custom.scss'
+import '../../node_modules/slick-carousel/slick/slick.css'
+import '../../node_modules/slick-carousel/slick/slick-theme.css'
+import '../../node_modules/aos/dist/aos.css'
 
-import "../assets/fonts/icon-font/css/style.css"
-import "../assets/fonts/fontawesome-5/css/all.css"
+import '../assets/fonts/icon-font/css/style.css'
+import '../assets/fonts/fontawesome-5/css/all.css'
 
 const tagManagerArgs = {
-  gtmId: "GTM-MP7MZVC",
+  gtmId: 'GTM-MP7MZVC',
 }
 
 if (process.browser) {
   TagManager.initialize(tagManagerArgs)
 }
 
-const MyApp = ({ Component, pageProps, router }) => {
+let footerCache
+
+const MyApp = ({ Component, pageProps, router, footer }) => {
+  useEffect(() => {
+    footerCache = footer
+  }, [])
+
   if (router.pathname.match(/sign|reset|coming/)) {
     return (
       <GlobalProvider>
-        <Layout pageContext={{ layout: "bare" }}>
+        <Layout pageContext={{ layout: 'bare' }}>
           <Component {...pageProps} />
         </Layout>
       </GlobalProvider>
@@ -32,11 +41,28 @@ const MyApp = ({ Component, pageProps, router }) => {
 
   return (
     <GlobalProvider>
-      <Layout pageContext={{}}>
+      <Layout pageContext={{}} footer={footer}>
         <Component {...pageProps} />
       </Layout>
     </GlobalProvider>
   )
+}
+
+const footerQuery = groq`
+*[_id == 'footer'][0] {
+  ...,
+}`
+
+MyApp.getInitialProps = async () => {
+  if (footerCache) {
+    return { footer: footerCache }
+  }
+
+  const footer = await getClient().fetch(footerQuery)
+
+  footerCache = footer
+
+  return { footer }
 }
 
 export default MyApp
