cc6025a79b097537f5b8d90c971c199288d3d757
izabellalarsson <izzwii.izabella@gmail.com>
2021-10-28 11:53:27 +0200

    chore: update schema

--- studio/schemas/pages/xPage.js
+++ studio/schemas/pages/xPage.js
@@ -3,7 +3,50 @@ export default {
   type: 'document',
   title: 'Om oss',
   fields: [
-    { type: 'hero', name: 'hero' },
+    {
+      name: 'hero',
+      type: 'object',
+      title: 'Hero',
+      options: {
+        collapsible: true,
+      },
+      fields: [
+        {
+          name: 'title',
+          title: 'Titel',
+          type: 'string',
+          validation: (Rules) =>
+            Rules.required().error('Du behöver sätta en rubrik.'),
+        },
+        {
+          name: 'subtitle',
+          title: 'Underrubrik',
+          type: 'text',
+          validation: (Rules) =>
+            Rules.required().error(
+              'En underrubrik kan vara snyggt att ha också.'
+            ),
+        },
+        {
+          type: 'title',
+          name: 'button',
+          title: 'Titel för knappen',
+          description:
+            'Knapp som leder till "Boka en gratis rådgivning" längre ner på sidan.',
+          validation: (Rules) =>
+            Rules.required().error('Lägg till text för knappen.'),
+        },
+        {
+          name: 'mediaType',
+          title: 'Media',
+          type: 'mediaType',
+          description:
+            'Här kan du lägga in vad du vill ha för hero genom att välja mellan att använda en bild, länka till en video eller välja en bakgrundsfärg.',
+          validation: (Rules) =>
+            Rules.required().error('Välj vad för typ av hero du vill ha.'),
+        },
+      ],
+    },
     {
       type: 'textGrid',
       name: 'textGrid',
