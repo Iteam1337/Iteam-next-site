0c3654be27368adab5b3796aea08c4e43bca41b1
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-11-10 11:30:07 +0100

    chore: sort desc

--- client/src/pages/case/index.js
+++ client/src/pages/case/index.js
@@ -95,7 +95,7 @@ const casePageQuery = groq`
   }`
 
 const casePostsQuery = groq`
-  *[_type == 'casePost'&& !(_id in path('drafts.**'))] | order(_createdAt)
+  *[_type == 'casePost'&& !(_id in path('drafts.**'))] | order(_createdAt desc)
   {
     ...,
   }`
