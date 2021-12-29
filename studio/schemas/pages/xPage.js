0f4c31e148511a7079ff2cdf1a02306ad848a013
izabellalarsson <izzwii.izabella@gmail.com>
2021-12-29 13:21:23 +0100

    use new hero

--- studio/schemas/pages/xPage.js
+++ studio/schemas/pages/xPage.js
@@ -3,56 +3,7 @@ export default {
   type: 'document',
   title: 'Om oss',
   fields: [
-    {
-      name: 'hero',
-      type: 'object',
-      title: 'Hero',
-      options: {
-        collapsible: true,
-      },
-      fields: [
-        {
-          name: 'title',
-          title: 'Titel',
-          type: 'string',
-          validation: (Rules) =>
-            Rules.required().error('Du behöver sätta en rubrik.'),
-        },
-        {
-          name: 'subtitle',
-          title: 'Underrubrik',
-          type: 'text',
-          validation: (Rules) =>
-            Rules.required().error(
-              'En underrubrik kan vara snyggt att ha också.'
-            ),
-        },
-        {
-          name: 'cta',
-          type: 'document',
-          title: 'Cta',
-          description:
-            'Knapp som leder till "Boka en gratis rådgivning" längre ner på sidan.',
-          fields: [
-            {
-              name: 'title',
-              type: 'string',
-              description: 'Ange text till CTA.',
-              title: 'Text på knappen',
-            },
-          ],
-        },
-        {
-          name: 'mediaType',
-          title: 'Media',
-          type: 'mediaType',
-          description:
-            'Här kan du lägga in vad du vill ha för hero genom att välja mellan att använda en bild, länka till en video eller välja en bakgrundsfärg.',
-          validation: (Rules) =>
-            Rules.required().error('Välj vad för typ av hero du vill ha.'),
-        },
-      ],
-    },
+    { type: 'hero', name: 'hero' },
     {
       type: 'textGrid',
       name: 'textGrid',
