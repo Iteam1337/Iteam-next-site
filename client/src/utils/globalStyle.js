3c2da1ab48057e282533fc93cd19ae5a70389bdf
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-08-17 15:56:57 +0200

    feat: add hidden header for clients section

--- client/src/utils/globalStyle.js
+++ client/src/utils/globalStyle.js
@@ -57,6 +57,18 @@ const globalStyle = createGlobalStyle`
     input[type="number"] {
       -moz-appearance: textfield; /* Firefox */
     }
+
+    .visibility-hidden {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      padding: 0;
+      margin: -1px;
+      overflow: hidden;
+      clip: rect(0, 0, 0, 0);
+      white-space: nowrap;
+      border-width: 0;
+    }
 `
 
 export default globalStyle
