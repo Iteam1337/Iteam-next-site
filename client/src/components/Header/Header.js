3eb8b7815de2200b8a431644ee8f303fe427ef0e
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-08-18 16:36:37 +0200

    feat/chore: add aria-labels to mobile menu buttons + rename components

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -112,7 +112,7 @@ const SiteHeader = styled.header`
   }
 `
 
-const ToggleButton = styled.button`
+const OpenMenuButton = styled.button`
   color: ${({ dark, theme }) => (dark ? '#fff' : '#000')} !important;
   border-color: ${({ dark, theme }) => (dark ? '#fff' : '#000')} !important;
 `
@@ -215,7 +215,7 @@ const DropdownMenu = styled.ul`
   }
 `
 
-const CrossWrapper = styled.button`
+const CloseMenuButton = styled.button`
   position: absolute;
   top: 11px;
   right: 24px;
@@ -411,7 +411,7 @@ const Header = ({ isDark = false }) => {
                 </Menu>
               </div>
             </div>
-            <ToggleButton
+            <OpenMenuButton
               className={`navbar-toggler btn-close-off-canvas ml-3 ${
                 gContext.visibleOffCanvas ? 'collapsed' : ''
               }`}
@@ -420,7 +420,7 @@ const Header = ({ isDark = false }) => {
               data-target="#mobile-menu"
               aria-controls="mobile-menu"
               aria-expanded="false"
-              aria-label="Toggle navigation"
+              aria-label="Öppna meny"
               onClick={toggleMenu}
               dark={isDark ? 1 : 0}
             >
@@ -438,7 +438,7 @@ const Header = ({ isDark = false }) => {
                   d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
                 />
               </svg>
-            </ToggleButton>
+            </OpenMenuButton>
           </nav>
         </Container>
       </SiteHeader>
@@ -446,11 +446,12 @@ const Header = ({ isDark = false }) => {
         show={gContext.visibleOffCanvas}
         onHideOffcanvas={gContext.toggleOffCanvas}
       >
-        <CrossWrapper
+        <CloseMenuButton
           onClick={() => {
             document.body.style.overflow = 'visible'
             gContext.toggleOffCanvas()
           }}
+          aria-label="Stäng meny"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -463,7 +464,7 @@ const Header = ({ isDark = false }) => {
           >
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
           </svg>
-        </CrossWrapper>
+        </CloseMenuButton>
         <NestedMenu menuItems={menuItems} />
       </Offcanvas>
     </>
