cd21c6814ddcd2acd5cfb931f3bc2cf257d208a2
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-09-12 14:18:54 +0200

    refactor: apply cslx

--- client/src/components/Core/Typography/Caption.js
+++ client/src/components/Core/Typography/Caption.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import clsx from 'clsx'
 
 export const Caption = ({ children, additionalClassName, light }) => {
   return (
     <p
-      className={`tw-mb-0 tw-text-sm tw-font-light tw-leading-tight tw-tracking-normal ${
-        light ? 'tw-text-gray-light' : 'tw-text-gray-dark'
-      } ${additionalClassName}`}
+      className={clsx(
+        'tw-mb-0 tw-text-sm tw-font-light tw-leading-tight tw-tracking-normal tw-text-gray-dark',
+        light && 'tw-text-gray-light',
+        additionalClassName
+      )}
     >
       {children}
     </p>
