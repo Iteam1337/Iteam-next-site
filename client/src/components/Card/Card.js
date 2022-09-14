4b7b810cf2bbc2697cfab73a3c1e99cc27722bea
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-09-14 10:27:17 +0200

    feat: set screen reader and visual order

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -6,10 +6,8 @@ import { useNextSanityImage } from 'next-sanity-image'
 const Card = ({ link, img, preTitle, title, children, readMore }) => {
   const imageProps = useNextSanityImage(client, img.asset._ref)
   return (
-    <li className="tw-group tw-relative focus-within:tw-shadow-xl hover:tw-shadow-xl">
-      <img {...imageProps} alt={img.alt} />
-      {preTitle && <Typography.Caption>{preTitle}</Typography.Caption>}
-      <h3 className="tw-peer">
+    <li className="tw-group tw-relative tw-flex tw-flex-col focus-within:tw-shadow-xl hover:tw-shadow-xl">
+      <h3 className="tw-peer tw-order-3">
         <a
           aria-label={`${title}, Läs mer…`}
           href={link}
@@ -18,11 +16,17 @@ const Card = ({ link, img, preTitle, title, children, readMore }) => {
           {title}
         </a>
       </h3>
-      <div>{children}</div>
+      {preTitle && (
+        <Typography.Caption additionalClassName="tw-order-2">
+          {preTitle}
+        </Typography.Caption>
+      )}
+      <img {...imageProps} alt={img.alt} className="tw-order-1" />
+      <div className="tw-order-4">{children}</div>
       {readMore && (
         <Typography.Caption
           ariaHidden="true"
-          additionalClassName="peer-focus-within:tw-underline group-hover:tw-underline"
+          additionalClassName="peer-focus-within:tw-underline group-hover:tw-underline tw-order-5"
         >
           Läs mer...
         </Typography.Caption>
