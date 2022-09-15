c942370e175a1cb5f73f1c498b49221e3d67a40f
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-09-15 17:55:43 +0200

    refactor: temporarily comment out refactor of Box component

--- client/src/components/Core/Box.js
+++ client/src/components/Core/Box.js
@@ -1,4 +1,6 @@
-import React from 'react'
+// todo: comment back this and fix related ui bugs
+
+/* import React from 'react'
 import clsx from 'clsx'
 
 const Box = ({ additionalClassName, children, ariaHidden }) => {
@@ -10,6 +12,35 @@ const Box = ({ additionalClassName, children, ariaHidden }) => {
       {children}
     </div>
   )
-}
+} */
+
+import styled from 'styled-components'
+import {
+  color,
+  space,
+  layout,
+  flexbox,
+  grid,
+  typography,
+  background,
+  border,
+  position,
+  shadow,
+} from 'styled-system'
+
+const Box = styled.div`
+  box-sizing: border-box;
+  min-width: 0;
+  ${color};
+  ${space};
+  ${layout};
+  ${flexbox};
+  ${grid};
+  ${typography};
+  ${background};
+  ${border};
+  ${position};
+  ${shadow};
+`
 
 export default Box
