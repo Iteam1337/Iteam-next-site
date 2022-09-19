58c0b10e6d5edf9a3a7a5053722a3ed273a1d9ef
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-09-19 15:01:36 +0200

    fix: set correct grid cols amount

--- client/src/components/CardList/CardListGrid.js
+++ client/src/components/CardList/CardListGrid.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const CardListGrid = ({ children }) => {
   return (
-    <ul className="tw-grid-flow-ro tw-flex tw-gap-8 sm:tw-grid-cols-2 md:tw-grid-cols-3 [@supports(display:grid)]:tw-grid">
+    <ul className="tw-flex tw-grid-flow-row tw-gap-8 md:tw-grid-cols-2 lg:tw-grid-cols-3 [@supports(display:grid)]:tw-grid">
       {children}
     </ul>
   )
