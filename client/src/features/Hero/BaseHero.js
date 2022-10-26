bfed0f77494465411cca40f4c6a61b2c05006a80
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-10-26 16:39:15 +0200

    refactor: create BaseHero component and use on iteamX page

---
+++ client/src/features/Hero/BaseHero.js
@@ -0,0 +1,105 @@
+import clsx from 'clsx'
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+import { CTALink, Section, Typography } from '../../components/Core'
+import { getExternalOrInternalLink, urlFor } from '../../utils/helpers'
+
+const SectionWithBackground = ({ mediaType, children, className }) => {
+  if (mediaType.type === 'color') {
+    return (
+      <Section
+        // todo: set bg-color as tailwind util class when Box component has been refactored
+        background={mediaType.color}
+        className={className}
+      >
+        {children}
+      </Section>
+    )
+  } else if (mediaType.type === 'image') {
+    return (
+      <Section
+        style={{
+          backgroundImage: `url(${urlFor(mediaType?.image.asset._ref)})`,
+        }}
+        className={clsx(
+          className,
+          'tw-relative tw-bg-cover tw-bg-center tw-bg-no-repeat'
+        )}
+      >
+        <div className="tw-relative tw-z-10">{children}</div>
+        <div
+          className={clsx(
+            className,
+            'tw-absolute tw-inset-0',
+            mediaType.darkGradient
+              ? 'tw-bg-gradient-to-l tw-from-[rgb(0,0,0,0.2)] tw-via-[rgb(0,0,0,0.4)] tw-to-[rgb(0,0,0,0.7)]'
+              : 'tw-bg-gradient-to-r tw-from-[rgb(255,255,255,0.9)] tw-via-[rgb(255,255,255,0.4)] tw-to-[rgb(255,255,255,0.1)]'
+          )}
+        />
+      </Section>
+    )
+  }
+  return (
+    <Section
+      className={clsx(
+        className,
+        'tw-bg-gradient-to-r tw-from-[rgb(141,141,236,0.17)] tw-to-[rgba(84,84,212,0)]'
+      )}
+    >
+      {children}
+    </Section>
+  )
+}
+
+export const BaseHero = ({
+  children,
+  subtitle,
+  mediaType,
+  link,
+  className,
+}) => {
+  return (
+    <SectionWithBackground
+      mediaType={mediaType}
+      className={clsx(
+        className,
+        'tw-flex tw-flex-col tw-justify-center tw-pt-32 tw-pb-14 lg:tw-pt-48 lg:tw-pb-48'
+      )}
+    >
+      <Container className="tw-h-full">
+        <Row>
+          <Col md="11" lg="9" xl="7">
+            {children}
+            {mediaType.type === 'color' ? (
+              <Typography.Paragraph className="tw-text-white">
+                {subtitle}
+              </Typography.Paragraph>
+            ) : (
+              <Typography.Paragraph className="tw-text-white">
+                <span className="tw-inline tw-whitespace-pre-wrap tw-bg-[rgb(0,0,0,0.7)] tw-box-decoration-clone tw-p-2 tw-leading-[2.46rem]">
+                  {subtitle}
+                </span>
+              </Typography.Paragraph>
+            )}
+            {link && (
+              <div
+                data-aos="fade-up"
+                data-aos-duration="300"
+                data-aos-once="true"
+                data-aos-delay="200"
+              >
+                <CTALink
+                  href={getExternalOrInternalLink(link.link)}
+                  text={link.title}
+                  className="tw-mt-8 tw-w-fit"
+                  variant="secondary"
+                />
+              </div>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </SectionWithBackground>
+  )
+}
--- client/src/features/Hero/Hero.js
+++ client/src/features/Hero/Hero.js
@@ -1,99 +1,26 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
 import clsx from 'clsx'
 
-import { Section, Typography, CTALink } from '../../components/Core'
-import { getExternalOrInternalLink, hexToRGBA } from '../../utils/helpers'
-import { urlFor } from '../../utils/helpers'
+import { Typography } from '../../components/Core'
+import { BaseHero } from './BaseHero'
 
-const SectionWithBackground = ({ mediaType, children }) => {
-  if (mediaType.type === 'color') {
-    const rgb = hexToRGBA(mediaType.color)
-    const colorArray = `${rgb[0]},${rgb[1]},${rgb[2]}`
-    return (
-      <Section
+export const Hero = ({ title, subtitle, mediaType, link, className }) => {
+  return (
+    <BaseHero
+      subtitle={subtitle}
+      mediaType={mediaType}
+      link={link}
+      className={className}
+    >
+      <Typography.H1
         className={clsx(
-          `tw-bg-gradient-to-r tw-from-[rgb(${colorArray},0.17)] tw-to-[rgb(84,84,212,0)]`,
-          'md:tw-pt-md:tw-pb-[null] tw-pt-[120px] tw-pb-[50px] md:tw-pb-[null] lg:tw-pt-[190px] lg:tw-pb-[180px]'
+          mediaType.darkGradient || mediaType.type === 'color'
+            ? 'tw-text-white'
+            : 'tw-text-gray-dark'
         )}
       >
-        {children}
-      </Section>
-    )
-  } else if (mediaType.type === 'image') {
-    return (
-      <Section
-        style={{
-          backgroundImage: `url(${urlFor(mediaType?.image.asset._ref)})`,
-        }}
-        className={
-          'tw-relative tw-bg-cover tw-bg-center tw-bg-no-repeat tw-pt-[120px] tw-pb-[50px] lg:tw-pt-[190px] lg:tw-pb-[180px]'
-        }
-      >
-        <div className="tw-relative tw-z-10">{children}</div>
-        <div
-          className={clsx(
-            'tw-absolute tw-inset-0',
-            mediaType.darkGradient
-              ? 'tw-bg-gradient-to-l tw-from-[rgb(0,0,0,.127)] tw-to-[rgb(0,0,0,0.8)]'
-              : 'tw-bg-gradient-to-r tw-from-[rgb(255,255,255,0.9)] tw-via-[rgb(255,255,255,0.4)] tw-to-[rgb(255,255,255,0.1)]'
-          )}
-        />
-      </Section>
-    )
-  }
-  return (
-    <Section
-      className={clsx(
-        'tw-bg-gradient-to-r tw-from-[rgb(141,141,236,0.17)] tw-to-[rgba(84,84,212,0)]',
-        'tw-pt-[120px] tw-pb-[50px]  lg:tw-pt-[190px] lg:tw-pb-[180px]'
-      )}
-    >
-      {children}
-    </Section>
-  )
-}
-
-export const Hero = ({ title, subtitle, mediaType, link }) => {
-  return (
-    <SectionWithBackground mediaType={mediaType}>
-      <Container>
-        <Row>
-          <Col lg="7">
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <Typography.H1
-                className={clsx(
-                  mediaType.type === 'image'
-                    ? mediaType.darkGradient
-                      ? 'tw-text-white'
-                      : 'tw-text-dark-gray'
-                    : 'tw-text-dark-gray'
-                )}
-              >
-                {title}
-              </Typography.H1>
-              <Typography.Paragraph className="tw-text-white">
-                <span className="tw-inline tw-whitespace-pre-wrap tw-bg-[rgb(0,0,0,0.7)] tw-box-decoration-clone tw-p-2 tw-leading-[2.46rem]">
-                  {subtitle}
-                </span>
-              </Typography.Paragraph>
-              {link && (
-                <CTALink
-                  href={getExternalOrInternalLink(link.link)}
-                  text={link.title}
-                  className="tw-mt-8 tw-w-fit"
-                  variant="secondary"
-                />
-              )}
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </SectionWithBackground>
+        {title}
+      </Typography.H1>
+    </BaseHero>
   )
 }
