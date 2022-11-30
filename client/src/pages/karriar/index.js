b3db9d1340f4e663f6a934aa1e6c9933c5ac404f
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-11-30 16:33:22 +0100

    fix: remove carousel from server side props

--- client/src/pages/karriar/index.js
+++ client/src/pages/karriar/index.js
@@ -4,25 +4,25 @@ import { NextSeo } from 'next-seo'
 
 import { PageWrapper } from '../../components/PageWrapper'
 import { Hero } from '../../features/Hero'
-import { Testimonial } from '../../features/Testimonial'
+// import { Testimonial } from '../../features/Testimonial'
 import { Feature, Roles } from '../../features/Karriar'
 import { usePreviewSubscription } from '../../lib/sanity'
 import { getClient } from '../../lib/sanity.server'
 import {
   filterDataToSingleItem,
-  shuffleArray,
+  // shuffleArray,
   urlFor,
 } from '../../utils/helpers'
 import { ExitPreviewLink } from '../../components/ExitPreviewLink'
 
-const Career = ({ data, preview = false, carousel }) => {
+const Career = ({ data, preview = false /* , carousel */ }) => {
   const { data: previewData } = usePreviewSubscription(data?.careerPageQuery, {
     initialData: data?.careerPage,
     enabled: preview,
   })
 
   const post = filterDataToSingleItem(previewData, preview)
-  const { hero, openings, section, textGrid, metaTags } = post
+  const { hero, openings /* , section */, textGrid, metaTags } = post
 
   return (
     <>
@@ -62,7 +62,7 @@ const Career = ({ data, preview = false, carousel }) => {
             link={hero.link}
           />
         )}
-        <Testimonial coworker={true} content={{ section, carousel }} />
+        {/* <Testimonial coworker={true} content={{ section, carousel }} /> */}
         <Feature content={textGrid} />
         <Roles content={openings} openPositions={data.openPositions} />
       </PageWrapper>
@@ -111,18 +111,18 @@ export async function getServerSideProps({ preview = false }) {
 
   if (!openPositions) return { notFound: true }
 
-  const { coworkerCarouselOne, coworkerCarouselTwo } = filterDataToSingleItem(
-    careerPage,
-    preview
-  )
-  const coworkerCarousel = [coworkerCarouselOne, coworkerCarouselTwo]
+  // const { coworkerCarouselOne, coworkerCarouselTwo } = filterDataToSingleItem(
+  //   careerPage,
+  //   preview
+  // )
+  // const coworkerCarousel = [coworkerCarouselOne, coworkerCarouselTwo]
   return {
     props: {
       preview,
       data: { openPositions, careerPage, careerPageQuery },
-      carousel: shuffleArray(
-        coworkerCarousel[Math.floor(Math.random() * coworkerCarousel.length)]
-      ),
+      // carousel: shuffleArray(
+      //   coworkerCarousel[Math.floor(Math.random() * coworkerCarousel.length)]
+      // ),
     },
   }
 }
